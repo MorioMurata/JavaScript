1cef74bffb6d19222620637922e4922a6877687e
MorioMurata <109211405+MorioMurata@users.noreply.github.com>
2022-09-02 07:35:18 +0000

    [Update]JavaScript

---
+++ app5.js
@@ -0,0 +1,13 @@
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
+
+alert(99 + 99);
+alert(645 - 378);
+alert(95 * 87);
+alert(365 / 5);
